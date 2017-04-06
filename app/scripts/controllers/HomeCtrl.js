94e975ffa629a1265f5cfd82b59e9e838daeb4f6
Scott <scottmail88@gmail.com>
2017-04-06 16:13:11 +0000

    Checkpoint 2 Complete

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+     function HomeCtrl(Room) {
+         this.rooms = Room.all;
+
+     }
+ 
+     angular
+         .module('blocChat')
+         .controller('HomeCtrl', ['Room', HomeCtrl]);
+})();
