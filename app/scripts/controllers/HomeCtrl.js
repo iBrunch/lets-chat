8e305feb7b60e0dd6b0d02326f7243b957c04ec0
Scott <scottmail88@gmail.com>
2017-06-02 01:19:08 +0000

    Checkpoint 6 Complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,10 @@
 (function() {
-     function HomeCtrl(Room, Message, $uibModal, $scope) {
+     function HomeCtrl(Room, Message, $uibModal, $scope, $cookies) {
+         $scope.master = {};
          var $ctrl = this;
          $ctrl.rooms = Room.all;
          $ctrl.currentRoom = null;
+         $ctrl.currentUser = $cookies.get('blocChatCurrentUser');
          
          $scope.hoverIn = function(){
              this.timestamp = true;
@@ -25,9 +27,20 @@
             $ctrl.currentRoom = room;
             $ctrl.messages = Message.getByRoomId($ctrl.currentRoom.$id);
         };
+        
+        $ctrl.sendMessage = function () {
+            $ctrl.newMessage.roomId = $ctrl.currentRoom.$id;
+            $ctrl.newMessage.username = $ctrl.currentUser;
+            Message.send($ctrl.newMessage);
+            $scope.home.newMessage.content = "";
+        };
+        
+        $scope.reset = function() {
+          $scope.newMessage = angular.copy($scope.master);
+        };
      }
  
      angular
          .module('blocChat')
-         .controller('HomeCtrl', ['Room', 'Message', '$uibModal', '$scope', HomeCtrl]);
+         .controller('HomeCtrl', ['Room', 'Message', '$uibModal', '$scope', '$cookies', HomeCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,9 +3,15 @@
     var Message = {};
     var ref = firebase.database().ref().child("message");
     var messages = $firebaseArray(ref);
-        
+    Message.all = messages;
+    
     Message.getByRoomId = function (roomId) {
       return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    };
+    
+    Message.send = function(newMessage) {
+      newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+      messages.$add(newMessage);
     };
     
     return Message;
